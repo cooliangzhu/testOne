0707d30970c85af2ad8856cf0cea7e2a68e68805
cooliangzhu <cooliangzhu@163.com>
2016-07-15 11:50:59 +0800

    第一个活动

---
+++ activ/index.php
@@ -0,0 +1,22 @@
+<?php
+/***
+ * new pro
+*/
+
+/**
+* 
+*/
+class foo
+{
+	
+	function __construct()
+	{
+		
+	}
+	function sayhello()
+	{
+		echo "hello one";
+	}
+}
+$foo = new foo;
+$foo -> sayhello();
--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 /***
  * for git test. is over 改变多一些 On server. 
  * write on server
+ * we have a new activ
 */
 
 
